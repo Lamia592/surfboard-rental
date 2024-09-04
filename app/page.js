a96fac94126ae58927d80ab55f008ff76c9c93c6
Lamia592 <lamiaa.monier@gmail.com>
2024-09-04 10:39:02 +0200

    Update page.js

--- app/page.js
+++ app/page.js
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <Layout>
       <div className="flex flex-col items-center justify-center min-h-screen bg-cover bg-center bg-surfing bg-opacity-90 text-white text-center">
-        <h1 className="text-black text-5xl font-bold mb-4">Ride the Waves</h1>
+        <h1 className="text-black text-5xl font-bold mb-4">Leave Nobody Behind</h1>
         <p className="text-black text-xl mb-8">Find the perfect surfboard for your next adventure.</p>
         <div className="space-x-4">
           <Link className="bg-yellow-500 hover:bg-yellow-400 text-white px-6 py-3 rounded-md text-lg font-semibold" href="/ ">
